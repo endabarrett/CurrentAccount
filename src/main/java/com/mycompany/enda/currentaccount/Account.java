b3c5a1f47ab004c2819318ec912675151abc7ba1
Enda Barrett <Enda.Barrett@nuigalway.ie>
2016-09-20 15:58:52 +0100

    First Commit

---
+++ src/main/java/com/mycompany/enda/currentaccount/Account.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.enda.currentaccount;
+
+
+/**
+ *
+ * @author 0102835S
+ */
+public class Account {
+    
+    String name;
+    int balance;
+    
+    public Account(String name, int bal)
+    {
+        this.name = name;
+        this.balance = bal;
+    }
+    
+}
